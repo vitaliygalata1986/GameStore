82165b89599c09f7f92ae1a7cead6a967a114397
Vitaliy Galata <vitaliy.galata@tangramltd.com>
2025-11-10 15:48:30 +0200

    develop header section

--- plugins/blocks-gamestore/build/block-contact/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '4cde6b1128bb0120703b');
--- themes/gamestore/functions.php
+++ themes/gamestore/functions.php
@@ -11,16 +11,41 @@
 /**
  * Enqueue the CSS files.
  *
+ * @return void
  * @since 1.0.0
  *
- * @return void
  */
-function gamestore_styles() {
-	wp_enqueue_style(
-		'gamestore-style',
-		get_stylesheet_uri(),
-		[],
-		wp_get_theme()->get( 'Version' )
-	);
+function gamestore_styles()
+{
+    wp_enqueue_style('gamestore-general',get_template_directory_uri() . '/assets/css/gamestore.css',[],wp_get_theme()->get( 'Version' ));
+    wp_enqueue_script('gamestore-theme-related', get_template_directory_uri() . '/assets/js/gamestore.js', [], wp_get_theme()->get( 'Version' ), true);
 }
-add_action( 'wp_enqueue_scripts', 'gamestore_styles' );
+
+add_action('wp_enqueue_scripts', 'gamestore_styles');
+
+function gamestore_google_font()
+{
+    $font_url = '';
+    $font = 'Urbanist';
+    $font_extra = 'ital,wght@0,400;0,700;1,400;1,700';
+
+
+    if ('off' !== _x('on', 'Google font: on or off', 'gamestore')) {
+        $query_args = array(
+            'family' => urldecode($font . ':' . $font_extra),
+            'subset' => urldecode('latin,latin-ext'),
+            'display' => urldecode('swap'),
+        );
+        $font_url = add_query_arg($query_args, '//fonts.googleapis.com/css2');
+    }
+
+    return $font_url;
+}
+
+function gamestore_google_font_script(){
+    wp_enqueue_style('gamestore-google-font',gamestore_google_font(),[],'1.0.0');
+}
+add_action('wp_enqueue_scripts','gamestore_google_font_script');
+
+// https://www.youtube.com/watch?v=c8nob2rQ9To&t=36638s
+// 3-38
