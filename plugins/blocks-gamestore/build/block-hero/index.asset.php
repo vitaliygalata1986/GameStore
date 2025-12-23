efc74700e69fc7f7bfceb38a7cb8fcfeb7222704
Виталий <36174158+vitaliygalata1986@users.noreply.github.com>
2025-12-24 01:38:49 +0300

    build all plugins (#18)
    Co-authored-by: Vitaliy Galata <vitaliy.galata@tangramltd.com>

--- plugins/blocks-gamestore/build/block-hero/index.asset.php
+++ plugins/blocks-gamestore/build/block-hero/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'c0882c7318011cfd70c9');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '12ca85e82c36a9384f21');
