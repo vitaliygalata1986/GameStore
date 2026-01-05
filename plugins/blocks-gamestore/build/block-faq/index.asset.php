e6ef734af65a15ea6333fa51aa2f0d2f7a3f45e7
Vitaliy Galata <vitaliy.galata@tangramltd.com>
2026-01-05 16:56:45 +0200

    feature/dev block footer

---
+++ plugins/blocks-gamestore/build/block-faq/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '939352df9e80ce01bf84');
---
+++ plugins/blocks-gamestore/build/block-faq/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'b8368793a5391431406a');
