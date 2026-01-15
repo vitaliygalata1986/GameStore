c6a964d6ec691dd257d9ad846da737f90e4415f5
Vitaliy Galata <vitaliy.galata@tangramltd.com>
2026-01-15 10:52:32 +0200

    build all plugins

--- plugins/blocks-gamestore/build/block-single-news/index.asset.php
+++ plugins/blocks-gamestore/build/block-single-news/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n', 'wp-server-side-render'), 'version' => 'ef39045e6afe24b16db9');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n', 'wp-server-side-render'), 'version' => '9f3141ef20633bca9766');
